a94df2ef389019970a12ea516f2ec95f268c5bdf
salientype <jorge@salientype.com>
2019-10-14 17:36:41 -0400

    added clear icons function

--- callbacks/rps.js
+++ callbacks/rps.js
@@ -1,5 +1,7 @@
 var countdown = function () {
-
+    
+    clearIconColors();
+    
     var num = 4;
     var timer = document.getElementById("countdown");
     var setTimer = setInterval(function () {
@@ -8,6 +10,7 @@ var countdown = function () {
         if (num == -1) {
             clearInterval(setTimer);
             startGame();
+            timer.innerHTML = "";
         }
     }, 1000);
 
@@ -60,23 +63,60 @@ var machineSelect = function () {
 
 var evalMatch = function (playerChoice, machineChoice) {
     
+    var playerBox = document.getElementById("playerChoice");
+    var machineBox = document.getElementById("machineChoice");
+    
     if (playerChoice == "rock" && machineChoice == "paper") {
         console.log("machine wins!");
+        clearIconColors();
+        machineBox.classList.add("text-success");
+        playerBox.classList.add("text-danger");
     } else if (playerChoice == "paper" && machineChoice == "rock") {
         console.log("human wins!");
+        clearIconColors();
+        machineBox.classList.add("text-danger");
+        playerBox.classList.add("text-success");
     } else if (playerChoice == "rock" && machineChoice == "scissors") { 
         console.log("human wins!");
+        clearIconColors();
+        machineBox.classList.add("text-danger");
+        playerBox.classList.add("text-success");
     } else if (playerChoice == "scissors" && machineChoice == "rock") {
         console.log("machine wins!");
+        clearIconColors();
+        machineBox.classList.add("text-success");
+        playerBox.classList.add("text-danger");
     } else if (playerChoice == "scissors" && machineChoice == "paper") {
         console.log("human wins!");
+        clearIconColors();
+        machineBox.classList.add("text-danger");
+        playerBox.classList.add("text-success");
     } else if (playerChoice == "paper" && machineChoice == "scissors") {
         console.log("machine wins!");
+        clearIconColors();
+        machineBox.classList.add("text-success");
+        playerBox.classList.add("text-danger");
     } else if (playerChoice == machineChoice) {
-        console.log("Tied Match!")
+        console.log("Tied Match!");
+        clearIconColors();
+        machineBox.classList.add("text-warning");
+        playerBox.classList.add("text-warning");
     } else {
         reset();
     }
+
+}
+
+var clearIconColors = function () {
+    
+    var iconBoxes = document.getElementsByClassName("icon-box");
+
+    for(var i = 0; i < iconBoxes.length; i++) {
+        iconBoxes[i].classList.remove("text-success");
+        iconBoxes[i].classList.remove("text-danger");
+        iconBoxes[i].classList.remove("text-warning");
+    }
+
 
 }
 
