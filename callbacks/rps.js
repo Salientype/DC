6dd9802570d1ae948fa9aff0e7c46fae23fe274f
salientype <jorge@salientype.com>
2019-10-14 01:12:47 -0400

    rock/paper/scissors halfway done

--- callbacks/rps.js
+++ callbacks/rps.js
@@ -1,11 +1,66 @@
-var countdown = function() {
+var countdown = function () {
     var num = 4;
     var timer = document.getElementById("countdown");
-    var setTimer = setInterval(function(){
+    var setTimer = setInterval(function () {
         timer.innerHTML = num;
         num--;
         if (num == -1) {
             clearInterval(setTimer);
-        } 
+            startGame();
+        }
     }, 1000);
+}
+
+
+var startGame = function () {
+    console.log(playerSelect());
+    console.log(machineSelect());
+}
+
+var playerSelect = function () {
+
+    var choice = document.querySelector('input[name="playerChoice"]:checked').value;
+
+    if (choice == "rock") {
+        // display rock in div
+        return choice;
+    } else if (choice == "paper") {
+        // display paper in div
+        return choice;
+    } else if (choice == "scissors") {
+        // display scissors in div
+        return choice;
+    } else {
+        reset();
+    }
+}
+
+var machineSelect = function () {
+
+    var choice = Math.floor((Math.random() * 3) + 1);
+
+    if (choice == 1) {
+        // display rock in div
+        return "rock";
+    } else if (choice == 2) {
+        // display rock in div
+        return "paper";
+    } else if (choice == 3) {
+        // display rock in div
+        return "scissors";
+    } else {
+        reset();
+    }
+}
+
+var evalMatch = function () {
+
+}
+
+var logMatch = function () {
+
+}
+
+var reset = function () {
+
 }
