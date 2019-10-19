d33c03625314b519a93d5861140126f32be0a2ad
salientype <jorge@salientype.com>
2019-10-18 21:49:19 -0400

    file setup and js

---
+++ social_feed/social-feed.js
@@ -0,0 +1,25 @@
+var init = function() {
+
+    // save form elements into vars
+    var mainContainer = document.getElementById("main-container");
+    
+    var editPostContainer = document.getElementById("edit-post-container");
+    var postForm = document.getElementById("post-form");
+    var editPostUser = document.getElementById("edit-post-user");
+    var editPostTitle = document.getElementById("edit-post-title");
+    var editPostText = document.getElementById("edit-post-text");
+    var editPostImgUrl = document.getElementById("edit-post-img-url");
+    var submitBtn = document.getElementById("edit-post-img-url");
+
+    var feedContainer = document.getElementById("feed-container");
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+    init();
+
+});
+
